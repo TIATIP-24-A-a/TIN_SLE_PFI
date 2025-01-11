0737c29f887a0a4e1e7ad32479c2bb565ba18970
timineichen <tim.ineichen@edu.teko.ch>
2025-01-11 15:35:14 +0100

    added: tests for find_coprime.py

---
+++ tests/test_find_coprime.py
@@ -0,0 +1,20 @@
+import unittest
+
+from functions.find_coprime import find_coprime
+
+
+class FindCoprimeTests(unittest.TestCase):
+    def test_find_coprime_of_12(self):
+        phi = 12
+        result = find_coprime(phi)
+        is_result_in_range = 2 <= result <= phi - 1
+        self.assertEqual(is_result_in_range, True)
+
+    def test_find_coprime_of_3(self):
+        phi = 3
+        result = find_coprime(phi)
+        is_result_in_range = 2 <= result <= phi - 1
+        self.assertEqual(is_result_in_range, True)
+
+if __name__ == '__main__':
+    unittest.main()
