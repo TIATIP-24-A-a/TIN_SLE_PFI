3ffcb4ec85204f034de8689fa72f06d0bc0a774f
Pedro <pedro.figueiredo@edu.teko.ch>
2025-01-11 15:51:29 +0100

    changed: large_prime function

--- functions/generate_large_prime.py
+++ functions/generate_large_prime.py
@@ -3,7 +3,7 @@ from functions.is_prime import is_prime
 
 
 def generate_large_prime(bits=512):
-    """Generiert eine große Primzahl mit einer angegebenen Bitlänge."""
+    """Generiert eine grosse Primzahl mit einer angegebenen Bitlänge."""
     while True:
         num = random.getrandbits(bits) | 1  # Sicherstellen, dass die Zahl ungerade ist
         if is_prime(num):
