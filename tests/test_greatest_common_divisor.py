d5fb4833a54616e915ebc8860c452946e074e843
timineichen <tim.ineichen@edu.teko.ch>
2025-01-11 15:36:28 +0100

    added: tests for greatest_common_divisor.py

---
+++ tests/test_greatest_common_divisor.py
@@ -0,0 +1,19 @@
+import unittest
+from functions.greatest_common_divisor import greatest_common_divisor
+
+
+class GreatestCommonDivisorTests(unittest.TestCase):
+    def test_greatest_common_divisor_of_6_9(self):
+        a = 6
+        b = 9
+        result = greatest_common_divisor(a, b)
+        self.assertEqual(3, result)  # add assertion here
+
+    def test_greatest_common_divisor_of_2_9(self):
+        a = 2
+        b = 9
+        result = greatest_common_divisor(a, b)
+        self.assertEqual(1, result)  # add assertion here
+
+if __name__ == '__main__':
+    unittest.main()
