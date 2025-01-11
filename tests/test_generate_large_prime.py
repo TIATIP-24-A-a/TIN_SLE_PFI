6a0a82a704ab709dcdeb02d8177e874549363e60
timineichen <tim.ineichen@edu.teko.ch>
2025-01-11 15:36:17 +0100

    added: tests for generate_large_prime.py

---
+++ tests/test_generate_large_prime.py
@@ -0,0 +1,13 @@
+import unittest
+from functions.generate_large_prime import generate_large_prime
+from functions.is_prime import is_prime
+
+
+class GenerateLargePrimeTests(unittest.TestCase):
+    def test_generate_large_prime(self):
+        result = generate_large_prime(512)
+        number = is_prime(result)
+        self.assertEqual(True, number)  # add assertion here
+
+if __name__ == '__main__':
+    unittest.main()
